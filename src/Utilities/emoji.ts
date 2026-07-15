18a869ab05f6a6d2ce8241cc0d960619cf40360f
Vireon <on96852@gmail.com>
2026-07-15 21:42:30 +0400

    Update emoji.ts

--- src/Utilities/emoji.ts
+++ src/Utilities/emoji.ts
@@ -8,6 +8,12 @@ export const EMOJI = Object.freeze({
   verify: "<a:Verify:1517572452081729667>",
   staff: "<:Staff:1520891667136254105>",
   loading: "<a:Loading:1517569264817672412>",
+  new1: "<:new1:1527005146188349492>",
+  new2: "<:new2:1527005092660904147>",
+  right: "<a:pointright:1527005041679143034>",
+  left: "<a:pointleft:1527004977933848770>",
+  thumbsup: "<:thumbsup:1527004918718660798>",
+  thumb down: "<:thumbsdown:1527004837093314612>",
 } as const);
 
 export type EmojiName = keyof typeof EMOJI;
